e2a170f19860e7a548d6078d2b54ca19c454321b
narin <hnr8652@gmail.com>
2024-09-16 20:00:38 +0900

    narin

--- src/main/java/com/project/oneshot/inventory/supplier/SupplierService.java
+++ src/main/java/com/project/oneshot/inventory/supplier/SupplierService.java
@@ -3,8 +3,12 @@ package com.project.oneshot.inventory.supplier;
 import com.project.oneshot.command.SupplierVO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SupplierService {
     public void registerSupplier(SupplierVO supplier);
     public List<SupplierVO> getAllSuppliers();
+    public List<SupplierVO> searchSuppliers(Map<String, Object> parameters);
+    SupplierVO getSupplierByNo(Long supplierNo);
+    public boolean modifySupplier(SupplierVO supplierVO);
 }
