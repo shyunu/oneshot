071c7135c5c20ad14045389a2385dc148bcbbe43
narin <hnr8652@gmail.com>
2024-09-20 09:59:26 +0900

    narin

--- src/main/java/com/project/oneshot/controller/PurchaseController.java
+++ src/main/java/com/project/oneshot/controller/PurchaseController.java
@@ -1,6 +1,8 @@
 package com.project.oneshot.controller;
 
 import com.project.oneshot.command.PurchaseVO;
+import com.project.oneshot.inventory.purchase.PurchaseCriteria;
+import com.project.oneshot.inventory.purchase.PurchasePageVO;
 import com.project.oneshot.inventory.purchase.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,10 +20,15 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping("/purchaseList")
-    public String purchase(Model model) {
-        List<PurchaseVO> list = purchaseService.getAllPurchase();
+    public String purchase(Model model, PurchaseCriteria cri) {
+        List<PurchaseVO> list = purchaseService.getAllPurchase(cri);
         System.out.println("list :" + list.toString());
         model.addAttribute("list", list);
+
+        int totalPurchase = purchaseService.getTotalPurchase(cri);
+        PurchasePageVO pageVO = new PurchasePageVO(cri, totalPurchase);
+        model.addAttribute("pageVO", pageVO);
+
         return "inventory/purchase";
     }
 }
