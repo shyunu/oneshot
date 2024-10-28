062571a36779884d6781c72eef7f5ca3dad77941
narin <hnr8652@gmail.com>
2024-10-28 10:50:38 +0900

    Inventory

--- src/main/java/com/project/oneshot/app/inventory/AppInventoryService.java
+++ src/main/java/com/project/oneshot/app/inventory/AppInventoryService.java
@@ -1,4 +1,26 @@
 package com.project.oneshot.app.inventory;
 
+import com.project.oneshot.command.*;
+
+import java.util.List;
+
 public interface AppInventoryService {
+
+    SupplierVO getSupplier(int supplierNo);
+
+//    public void registerPurchase(List<PurchaseVO> list);
+
+    List<SupplierVO> getAllSuppliers();
+
+    List<CategoryVO> getCategories(int supplierNo);
+
+    List<ProductVO> getProducts(int supplierNo, int categoryNo);
+
+    ProductVO getQuantity(int productNo);
+
+    List<ProductVO> getProductsByCategory(Long categoryNo);
+
+    List<EmployeeVO> getAllEmployees();
+
+    List<CategoryVO> getAllCategories();
 }
