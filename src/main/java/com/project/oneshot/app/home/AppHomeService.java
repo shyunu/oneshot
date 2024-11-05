a910b1c575ab3f1ef964026469a0be38b062f00e
suhyun-kim9 <ksh020674@gmail.com>
2024-11-05 16:55:30 +0900

    home수정중

--- src/main/java/com/project/oneshot/app/home/AppHomeService.java
+++ src/main/java/com/project/oneshot/app/home/AppHomeService.java
@@ -1,16 +1,13 @@
 package com.project.oneshot.app.home;
 
-import com.project.oneshot.command.ClientVO;
-import com.project.oneshot.command.ContractVO;
-import com.project.oneshot.command.OrderItemVO;
-import com.project.oneshot.command.OrderVO;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import java.util.Map;
 
 public interface AppHomeService {
 
     int getContractCount(); //금일 계약 등록건수 조회
     int getSalesCount(); //금일 판매 등록건수 조회
+
+    List<Map<String, Object>> getQuarterlyOrderAmount(); //올해 분기별 매출액
 
 }
