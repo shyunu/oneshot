ccafa6de1b3e7f009f0cfc102201bf8bf1361cdc
suhyun-kim9 <ksh020674@gmail.com>
2024-11-06 14:04:27 +0900

    home

--- src/main/java/com/project/oneshot/app/home/AppHomeRestController.java
+++ src/main/java/com/project/oneshot/app/home/AppHomeRestController.java
@@ -3,6 +3,9 @@ package com.project.oneshot.app.home;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/homeApp")
@@ -27,4 +30,11 @@ public class AppHomeRestController {
         System.out.println("판매건수: " + salesCount);
         return salesCount;
     }
+
+    // 분기별 판매 총액
+    @GetMapping("/getQuarterlyOrderAmount")
+    public List<Map<String, Object>> getQuarterlyOrderAmount() {
+        return appHomeService.getQuarterlyOrderAmount();
+    }
+
 }
